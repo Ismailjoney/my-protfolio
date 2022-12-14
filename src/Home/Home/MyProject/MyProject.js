68b8c2aaa7ce4e96e8e5911961dc4abece17e153
Ismailjoney <mohammadismailjoney@gmail.com>
2022-12-14 20:47:27 +0600

    finish 2 link use

--- src/Home/Home/MyProject/MyProject.js
+++ src/Home/Home/MyProject/MyProject.js
@@ -6,7 +6,7 @@ import ProjectDetails from './ProjectDetails';
 const MyProject = () => {
     const [projects, setProjects] = useState([])
     useEffect(() => {
-        fetch(`http://localhost:5000/projects`)
+        fetch(`https://protfolio-server-nine.vercel.app/projects`)
             .then(res => res.json())
             .then(data => setProjects(data))
     }, [])
@@ -14,7 +14,7 @@ const MyProject = () => {
     return (
         <div  className='mt-35 mb-14 font-bold'>
             <h1 className='text-3xl tracking-widest mt-32 my-22'>My Projects</h1>
-                <div className='grid sm:grid-cols-1  md:grid-cols-3 lg:grid-cols-3 mt-5 justify-center items-center'>
+                <div className='grid justify-items-center sm:grid-cols-1  md:grid-cols-3 lg:grid-cols-3 mt-5  '>
                     {
                         projects.map(project => <ProjectDetails
                             id={project.id}
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -23,7 +23,7 @@ export const route = createBrowserRouter([
             {
                 path:'/detailsinfo/:id',
                 element:<DetailsInformation></DetailsInformation>,
-                loader: ({params}) => fetch(`http://localhost:5000/projects/${params.id}`)
+                loader: ({params}) => fetch(`https://protfolio-server-nine.vercel.app/projects/${params.id}`)
             },
             {
                 path:'/about',
